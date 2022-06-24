1e9111207a5f03a349f5e494fdefe5d09b7657fa
Krstnxx <magsipoccristineann@gmail.com>
2022-06-24 16:26:29 +0800

    added

--- maintenance.js
+++ maintenance.js
@@ -4,3 +4,13 @@ setInterval(() => {
   const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
   progressBar.style.setProperty('--width', width + .1)
 }, 5)
+
+
+var modal = document.getElementById('id01');
+	
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+  modal.style.display = "modal";
+  }
+}
